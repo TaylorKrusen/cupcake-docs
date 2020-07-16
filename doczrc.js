f1c92f2cf5462e536d3706d59ced0262d54e8391
Dan Aprahamian <daprahamian@dropbox.com>
2020-07-16 14:39:53 -0400

    Sets the order of the top level menu

--- doczrc.js
+++ doczrc.js
@@ -3,5 +3,10 @@ module.exports = {
     "src": "src/mdx",
     "source": "src/mdx",
     "plugins": ['gatsby-plugin-mdx'],
-    "typescript": true
+    "typescript": true,
+    "menu": [
+        { name: "Introduction", menu: []},
+        { name: "User Endpoints", menu: []},
+        { name: "Business Endpoints", menu: []}
+    ]
 }
