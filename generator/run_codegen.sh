c79ab80d9de7e303b1a9a97e73b3f252b5e1a0b9
Taylor Krusen <taylork@dropbox.com>
2020-07-16 15:43:40 -0700

    added file type files

--- generator/run_codegen.sh
+++ generator/run_codegen.sh
@@ -2,4 +2,4 @@
 
 # Quick wrapper for how to run my code generator
 # Will be edited as necessary (e.g. with the correct path)
-PYTHONPATH=stone python -m stone.cli -a:all -w check -w file_properties -w sharing -w team -w auth -w account mdx.stoneg.py ../src spec/*.stone
+PYTHONPATH=stone python -m stone.cli -a:all -w check -w files -w file_properties -w sharing -w team -w auth -w account mdx.stoneg.py ../src spec/*.stone
