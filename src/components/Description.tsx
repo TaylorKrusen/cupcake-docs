2d04aa35f67e64c5f1381b6cc4025e708db173d5
Thomas Hsu <thomash@dropbox.com>
2020-07-16 17:06:31 -0700

    Fixed scroll bar issue and added key property to Description

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Description: React.FC<{description: string}> = ({description}) => {
   const paragraphs = description.split('&nbsp').join(' ').split('\\n');
-  const toBeRendered = paragraphs.map((paragraph) => {
-    return <div>{paragraph}</div>;
+  const toBeRendered = paragraphs.map((paragraph, idx) => {
+    return <div key={`description-paragraph-${idx}`}>{paragraph}</div>;
   });
   return <div className="description-box">{toBeRendered}</div>;
 };
